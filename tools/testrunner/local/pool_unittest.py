fbf0a570fcb0c02c8a7e983fe24b678b925242a7
Vadim Gorbachev (bmsdave) <bmsdave@gmail.com>
2019-02-05 08:12:50 +0300

    fix queue is not defined

--- tools/testrunner/local/pool_unittest.py
+++ tools/testrunner/local/pool_unittest.py
@@ -3,6 +3,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# for py2/py3 compatibility
+try:
+  from queue import Queue # Python 3
+except NameError:
+  from Queue import Queue # Python 2
+
 import os
 import sys
 import unittest
